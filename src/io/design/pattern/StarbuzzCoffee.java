ae1075cadf14e7402a1f63a68f76781fd602d8d0
unknown <hamza.ellouni@gmail.com>
2020-04-16 01:38:32 +0200

    first commit

--- src/io/design/pattern/StarbuzzCoffee.java
+++ src/io/design/pattern/StarbuzzCoffee.java
@@ -16,21 +16,21 @@ public class StarbuzzCoffee {
 
         System.out.println(tea.cost());
 
-//        Beverage beverage = new Espresso();
-//        System.out.println(beverage.getDescription()
-//                + " $" + beverage.cost());
-//        Beverage beverage2 = new DarkRoast();
-//        beverage2 = new Mint(beverage2);
-//        beverage2 = new Mint(beverage2);
-//        beverage2 = new Whip(beverage2);
-//        System.out.println(beverage2.getDescription()
-//                + " $" + beverage2.cost());
-//        Beverage beverage3 = new HouseBlend();
-//        beverage3 = new Milk(beverage3);
-//        beverage3 = new Mint(beverage3);
-//        beverage3 = new Whip(beverage3);
-//        System.out.println(beverage3.getDescription()
-//                + " $" + beverage3.cost());
+        Beverage beverage = new Espresso();
+        System.out.println(beverage.getDescription()
+                + " $" + beverage.cost());
+        Beverage beverage2 = new DarkRoast();
+        beverage2 = new Mint(beverage2);
+        beverage2 = new Mint(beverage2);
+        beverage2 = new Whip(beverage2);
+        System.out.println(beverage2.getDescription()
+                + " $" + beverage2.cost());
+        Beverage beverage3 = new HouseBlend();
+        beverage3 = new Milk(beverage3);
+        beverage3 = new Mint(beverage3);
+        beverage3 = new Whip(beverage3);
+        System.out.println(beverage3.getDescription()
+                + " $" + beverage3.cost());
 
     }
 }
